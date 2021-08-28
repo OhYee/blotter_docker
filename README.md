f5682c5f13c7b251c6a88fa89831fe3a56807a64
OhYee <oyohyee@oyohyee.com>
2021-08-29 02:06:11 +0800

    feat: 从服务端拉取镜像运行

--- README.md
+++ README.md
@@ -6,6 +6,18 @@
 - [后端 OhYee/blotter](https://github.com/OhYee/blotter)
 - [前端 OhYee/blotter_page](https://github.com/OhYee/blotter_page)
 
+## 使用
+
+```bash
+# 拉取最新版镜像
+bash script.bash pull
+
+# 启动服务
+bash script.bash start
+
+# 关闭服务
+bash script.bash stop
+```
 ## TODO
 
 - [x] Docker 编译镜像
@@ -14,3 +26,7 @@
 - [ ] 定时任务脚本自动配置
 - [ ] 完善文档
 - [ ] 自动部署
+
+## 公网访问
+
+默认监听 50000 端口，同时在 80 端口监听 www.example.com 域名，可配置 nginx/conf.d 相关文件修改域名及 HTTPS
--- script.bash
+++ script.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export backendImage="ohyee/blotter:cef7a23"
+export frontImage="ohyee/blotter_page:eab3a5481"
+
 func_check_docker() {
     DOCKER_PATH=$(which docker)
     if [[ $? -eq 0 ]]; then
@@ -30,6 +33,19 @@ func_build_frontend() {
     cd blotter_page && bash ./build.bash && cd ..
 }
 
+func_pull() {
+    echo "Pulling blotter"
+    docker pull ${backendImage}
+    docker pull ${frontImage}
+}
+
+func_push() {
+    echo "Pushing blotter"
+    docker push ${backendImage}
+    docker push ${frontImage}
+}
+
+
 func_start() {
     echo "Starting blotter"
 
@@ -45,11 +61,13 @@ func_stop() {
 func_help() {
     echo "Blotter Docker 环境"
     echo ""
-    echo "1. 安装 docker、docker-compose 环境"
-    echo "2. 需要运行 bash script.bash init 初始化前后端代码"
-    echo "3. bash script.bash build 构建前后端镜像（构建后可以删除前后端源码（但是没必要）"
-    echo "4. bash script.bash start 启动 blotter 前后端程序"
-    echo "5. bash script.bash stop 停止前后端按程序"
+    # echo "1. 安装 docker、docker-compose 环境"
+    # echo "2. 需要运行 bash script.bash init 初始化前后端代码"
+    # echo "3. bash script.bash build 构建前后端镜像（构建后可以删除前后端源码（但是没必要）"
+    # echo "4. bash script.bash start 启动 blotter 前后端程序"
+    # echo "5. bash script.bash stop 停止前后端程序"
+    echo "bash script.bash pull     拉取镜像"
+    echo "bash script.bash start    启动服务镜像"
     echo ""
     echo "mongodb 数据存储在 data 目录中、nginx 配置文件存储在 conf.d 文件夹中，可根据需要自定义 HTTPS"
 }
@@ -67,6 +85,8 @@ func_update() {
 case $1 in
     "check")    func_check_docker   $@;;
     "update")   func_update         $@;;
+    "pull")   func_pull         $@;;
+    "push")   func_push         $@;;
     "backend")  func_check_docker && func_build_backend  $@;;
     "frontend") func_check_docker && func_build_frontend $@;;
     "build")    func_check_docker && func_build_backend && func_build_frontend $@;;
